37151b880d22dbafaea602db2773cd2efdac6545
Youri Bakker <youriboulanger@gmail.com>
2021-09-09 14:36:08 +0200

    done

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Linter_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(22, 41);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(155, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(183, 39);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(22, 97);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(236, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(286, 208);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PHP tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linter_Tester
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            GetPath();
+        }
+        string startupPath;
+        string pathPhp;
+        
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pathPhp = "";
+                // this open the file explorer, this let's the user select a file
+                OpenFileDialog pathTo = new OpenFileDialog();
+                // you can only select php files nothing else
+                pathTo.Filter = "PHP file | *.php;";
+                pathTo.FilterIndex = 1;
+                // you only can select 1 fil
+                pathTo.Multiselect = false;
+
+                if (pathTo.ShowDialog() == DialogResult.OK)
+                {
+                     pathPhp = pathTo.FileName;
+                     txtFilePath.Text = pathPhp;
+                }
+                //this if can't really break the program
+            }
+            catch (Exception) { 
+            
+            }
+        }
+
+        //get path to the root file of the Project
+        private void GetPath()
+        {
+            startupPath = Application.StartupPath;
+            startupPath = startupPath.Substring(0, startupPath.Length - 9);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            //the command \" adds a " to the command for the file path
+            strCmdText = "/K php \"" + startupPath + "XML/phpcs.phar\" " + "--standard=\"" + startupPath +"XML/PHP_lenient.xml\" "+ "\"" + pathPhp + "\"" + " && pause";
+             
+            //starts CMD and auto executes the command
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+            //resets the path to nothing so the user can test another file
+            pathPhp = "";
+        }
+    }
+       
+}
+
